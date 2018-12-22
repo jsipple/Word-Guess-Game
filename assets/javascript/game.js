406c5d95b1488e14b62a2ee2fd7adcc7535f73a8
Jacob Sipple <sipple3@gmail.com>
2018-12-22 10:23:04 -0500

    have working game but need to make it reload when won and live/guesses counter to function properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,59 @@
-window.addEventListener("keypress", checkKeyPress, false);
+document.addEventListener("keyup", checkKeyPress);
 
-function checkKeyPress (key) {
-    if (key.keyCode >= 97 && key.keyCode <= 122) {
-        document.getElementById("guessed").innerHTML;
+let word = ["Brandon Sanderson", "Dakota Krout", "Dennis Taylor", "Dean Kootz", "Stephen King", "Elizabeth Moon", "Mary Robinette Kowall"]
+  
+let lives = 10;
+let displayWriter = [];
+let randomNumber = Math.floor(Math.random() * word.length);
+let writer = word[randomNumber];
+let correct = writer.split("");
+let guessed = [];
+let incorrect = 0;
+let wins = 0
+// need to make this a function so that i can run again when person wins
+function randomWord() {
+    writer = word[randomNumber];
+}
+// change for loop into iterator by doing let = for ()
+function underscore() {
+    for (i = 0; i < writer.length; i++) {
+    if (writer[i] === " ") {
+        displayWriter[i] = "<br>";
+        correct[i] ="<br>";
+    } else {
+        displayWriter[i] = "_ ";
+    }
+}
+}
+underscore();
+document.getElementById("hangman").innerHTML = displayWriter.join("");
+
+function checkKeyPress(event) {
+    if (event.keyCode >= 65 && event.keyCode <= 90); {
+        guessed.push(event.key)
+        document.getElementById("guessed").innerHTML = guessed;
     }
 }
 
-document.getElementById("guessed").innerHTML
-
-let word = ["Brandon Sanderson", "Dakota Krout", "Dennis Taylor", "Dean Kootz", "Stephen King", "Elizabeth Moon", "Mary Robinette Kowall"]
-
-function randomword () {
-    let randomNumber = Math.floor(Math.random() * word.length);
-    document.getElementById('quote-display').innerHTML = word[randomNumber]
-}
+document.addEventListener("keydown", function checkCorrect() {
+    for (i = 0; i < correct.length; i++) {
+        if (event.key === correct[i].toLowerCase()) {
+            displayWriter[i] = correct[i];
+            document.getElementById("hangman").innerHTML = displayWriter.join("")
+        
+            if (displayWriter.join("") === correct.join("")) {
+            alert("You Win");
+            wins++
+            document.getElementById("wins").textContent = "Wins: " + wins;
+            randomWord();
+            underscore();
+        }
+    } 
+    }
+})
+correct.forEach(function(event, i) {
+    if (guessed[i] !== event) {
+        lives--;
+        document.getElementById("lives").textContent = "Current lives: " + lives
+    }
+})
